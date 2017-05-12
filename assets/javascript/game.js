6a8064528955eec41ecceb02e21a8b4a01dff183
Narin Sundarabhaya <nrs710@gmail.com>
2017-05-11 23:44:12 -0700

    re-organized functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,94 +13,131 @@
 //      -   add to wins
 //      -   start new game
 
-
-var teams = [
-    // ["B", "O", "S", "T", "O", "N", "R", "E", "D", "S", "O", "X"],
-    // ["B", "A", "L", "T", "I", "M", "O", "R", "E", "O", "R", "I", "O", "L", "E", "S"],
-    // ["T", "A", "M", "P", "A", "B", "A", "Y", "R", "A", "Y", "S"],
-    // ["L", "O", "S", "A", "N", "G", "E", "L", "E", "S", "D", "O", "D", "G", "E", "R", "S"],
-    // ["L", "O", "S", "A", "N", "G", "E", "L", "E", "S", "A", "N", "G", "E", "L", "E", "S"],
-    // ["A", "R", "I", "Z", "O", "N", "A", "D", "I", "A", "M", "O", "N", "D", "B", "A", "C", "K", "S"],
-    // ["K", "A", "N", "S", "A", "S", "C", "I", "T", "Y", "R", "O", "Y", "A", "L", "S"]
-    "BOSTON RED SOX",
-    "BALTIMORE ORIOLES",
-    "TAMPA BAY RAYS",
-    "LOS ANGELES DODGERS",
-    "LOS ANGELES ANGELES",
-    "ARIZONA DIAMONDBACKS",
-    "KANSAS CITY ROYALS"
-];
-
-var wins = 0;
-var remainGuess = 10;
-var guessedLetters = [];
-var loses = 0;
+var losses = 0;
 var wins = 0;
 var blanks = "";
+var guessedLetters = [];
+var successLetters = [];
+var wrongletters = [];
 
-// variable for randomly choosing current word
-var currentWord = function() {
-    return teams[Math.floor(Math.random() * teams.length)];
+var teams = [
+    "boston red sox",
+    "baltimore orioles",
+    "tampa bay rays",
+    "los angeles dodgers",
+    "los angeles angeles",
+    "arizona diamond backs",
+    "kansas city royals"
+];
+
+
+function startGame() {
+    //  Reset guessed letters array to 0 and remainingGuess to 10
+
+    var guesses = 0;
+
+    // Solution for randomly choosing current word
+    var currentWord = teams[Math.floor(Math.random() * teams.length)];
+
+    // Test - randomly choosing word
+    console.log("Test Random team:" + currentWord);
+
+    // Break currenWord string into array of letters
+    currentWord = currentWord.split("");
+
+    // count number of "_" needed
+    var blanks = currentWord.length;
+
+    // reset guess  & success arrays at each round
+    guessedLetters = [];
+    successLetters = [];
+
+    // reset wrong guesses array
+    wrongletters = [];
+
+    blankAndSuccesses = [];
+
+    // blanksAndSucessess list with appropirate number of blanks
+    for (var i = 0; i < blanks; i++) {
+        blankAndSuccesses.push("_");
+    }
+    // update html on page
+    blankAndSuccesses = blankAndSuccesses.join(", ");
+    // document.getElementById('wordBlanks').innerHTML = blankAndSuccesses;
+
+
+    // Tests
+    console.log("Test: team into array:" + currentWord);
+    console.log("Test: count # of _ needed: " + blanks);
+    console.log("Test: blanksAndSuccesses " + blankAndSuccesses)
+
 }
 
-// Test - randomly choosing word
-console.log(currentWord());
+// checkLetters() function
 
+function checkLetters(letter) {
 
+    var letterInWord = false;
+    // Check if a letter exists inside currenWord array
+    for (var i = 0; i < blanks; i++) {
+        if (letter === currentWord[i]) {
+            letterinWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < currentWord.length; i++) {
+            if (currentWord[i] === letter) {
 
-// updates HTML
-var updatedHtml = function(element) {
-    document.getElementById("guessedLetters").innerHTML = guessedLetters;
-    document.getElementById("remaingGuess").innerHTML = remainGuess;
-    document.getElementById("currentWord").innerHTML = currentWord();
+            }
+        }
+    }
 }
+
+function roundComplete() {
+    // initial test for status of game
+    console.log(wins, losses, guesses);
+
+    // update HTML to reflect number of guesses
+    document.getElementById('remainingGuesses').innerHTML = guesses;
+    // update HTML to reflect number of correct guesses
+    document.getElementById('correctGuesses').innerHTML = wordBlanks;
+    // update wordBlanks to show any correct guesses
+
+    // update #guessedLetters to show the wrong guesses
+    document.getElementById('guessedLetters').innerHTML = wrongLetteres;
+
+    // If we have gotten all the letters to match the solution...
+    // ..add to the win counter & give the user an alert.
+
+    // Update the win counter in the HTML & restart the game.
+    startGame();
+
+    // If we've run out of guesses..
+    // Add to the loss counter.
+    // Give the user an alert.
+    // Update the loss counter in the HTML.
+    // Restart the game.
+    startGame();
+
+}
+
+// once page loads, game starts
+startGame();
 
 // Starts the game on keypress & adds to guess array
 document.onkeyup = function(event) {
 
-    // Determines which key was pressed
-    var userGuess = event.key.toUpperCase();
+    // Determines which key was pressed & converts to lowercase
+    var userGuess = String.fromCharCode(event.key).toLowerCase();
 
-    // adds pressed keys to guessedLetters array
+    // adds pressed keys to guessedLetters array --- FROM OLD VERSION
     guessedLetters.push(userGuess);
     console.log(guessedLetters);
 
-    currentWord();
-    updatedHtml();
+    // runs the checkLetters function for correctness
+    checkLetters(userGuess);
 
-}
+    // Runs the function after each round is done.
+    roundComplete();
 
-// 
-//  RE-ORG For DeBug
-// 
-
-
-// //  Function for display of "_" - blank letters that have not been guessed
-// var blankWord = function() {
-//     for (var i = 0; i < currentWord.length; i++) {
-//         blanks = +"_";
-//     }
-// }
-
-// // Conditionals to check if userGuess is in teams array
-// var conditions = function(userGuess) {
-
-//     for (var i = 0; i < currentWord.length; i++) {
-//         if (userGuess === currentWord[i]) {
-
-//         }
-//     }
-
-// }
-
-// // adds to guessed letters if incorrect
-// var guessedWrong = function(userGuess) {
-//     if (userGuess !== teams) {
-//         guessedLetters.push(userGuess);
-//     }
-// }
-
-// // checks if guessed key already
-// var checkKey = function(userGuess) {
-//     if (userGuess === guessedLetters) {}
-// }
+};
